85768fb99968b40c9fab768c89c4e572642dba02
hmllk <763632135@qq.com>
2021-11-22 16:08:21 +0800

    Update ConvertController.cs

--- Convert/Controllers/ConvertController.cs
+++ Convert/Controllers/ConvertController.cs
@@ -65,5 +65,47 @@ namespace Convert.Controllers
             await _officeService.PdfToImage(filaName, pdfFile, strPath);
             return new JsonResult("ok");
         }
+        
+         [HttpPut("pdftoimg")]
+        public async Task<IActionResult> PDFToImgs(string key)
+        {
+            var formFile = HttpContext.Request.Form?.Files?.FirstOrDefault();
+            if (formFile == null)
+            {
+                throw new ApplicationException("上传文件不存在");
+            }
+
+            //保留到本地
+            var strPath = Path.Combine(AppContext.BaseDirectory, "wwwroot", $"{key}",
+                $"{DateTime.Now.ToString("yyyyMMddhhmmssfff")}");
+            if (!Directory.Exists(strPath))
+            {
+                Directory.CreateDirectory(strPath);
+            }
+
+            var path = Path.Combine(strPath, formFile.Name);
+
+            await using (var stream = new MemoryStream())
+            {
+                await formFile.CopyToAsync((Stream) stream, new CancellationToken());
+                var array = stream.ToArray();
+                if (!System.IO.File.Exists(path))
+                {
+                    await System.IO.File.WriteAllBytesAsync(path, array);
+                }
+            }
+
+            // //转为pdf
+            // _officeService.ConvertToPdf(path, strPath);
+
+            //转为img
+            var filaName=  Path.GetFileNameWithoutExtension(path);
+            
+            Console.WriteLine($"开始读取pdf{DateTime.Now.ToString()}");
+            //var pdfFile = Path.Combine(strPath, $"{filaName}.pdf");
+            await _officeService.PdfToImage(filaName, path, strPath);
+            Console.WriteLine($"pdf转换完成{DateTime.Now.ToString()}");
+            return new JsonResult("ok");
+        }
     }
 }
